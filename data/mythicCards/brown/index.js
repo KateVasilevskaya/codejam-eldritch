b80ef682163ea968f8490694edd069fdc7a6ae0b
KateVasilevskaya <7091764@gmail.com>
2022-08-28 17:48:09 +0200

    task is completed

--- data/mythicCards/brown/index.js
+++ data/mythicCards/brown/index.js
@@ -1,132 +1,177 @@
-import brownCardsAssets from '../../../assets/MythicCards/brown';
+// import brownCardsAssets from "../../../assets/MythicCards/brown";
+const brown1 = "../assets/MythicCards/brown/brown1.png";
+const brown2 = "../assets/MythicCards/brown/brown2.png";
+const brown3 = "../assets/MythicCards/brown/brown3.png";
+const brown4 = "../assets/MythicCards/brown/brown4.png";
+const brown5 = "../assets/MythicCards/brown/brown5.png";
+const brown6 = "../assets/MythicCards/brown/brown6.png";
+const brown7 = "../assets/MythicCards/brown/brown7.png";
+const brown8 = "../assets/MythicCards/brown/brown8.png";
+const brown9 = "../assets/MythicCards/brown/brown9.png";
+const brown10 = "../assets/MythicCards/brown/brown10.png";
+const brown11 = "../assets/MythicCards/brown/brown11.png";
+const brown12 = "../assets/MythicCards/brown/brown12.png";
+const brown13 = "../assets/MythicCards/brown/brown13.png";
+const brown14 = "../assets/MythicCards/brown/brown14.png";
+const brown15 = "../assets/MythicCards/brown/brown15.png";
+const brown16 = "../assets/MythicCards/brown/brown16.png";
+const brown17 = "../assets/MythicCards/brown/brown17.png";
+const brown18 = "../assets/MythicCards/brown/brown18.png";
+const brown19 = "../assets/MythicCards/brown/brown19.png";
+const brown20 = "../assets/MythicCards/brown/brown20.png";
+const brown21 = "../assets/MythicCards/brown/brown21.png";
+
+const brownCardsAssets = {
+  brown1,
+  brown2,
+  brown3,
+  brown4,
+  brown5,
+  brown6,
+  brown7,
+  brown8,
+  brown9,
+  brown10,
+  brown11,
+  brown12,
+  brown13,
+  brown14,
+  brown15,
+  brown16,
+  brown17,
+  brown18,
+  brown19,
+  brown20,
+  brown21,
+};
 
 const cardsData = [
   {
-    id: 'brown1',
+    id: "brown1",
     cardFace: brownCardsAssets.brown1,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown2',
+    id: "brown2",
     cardFace: brownCardsAssets.brown2,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown3',
+    id: "brown3",
     cardFace: brownCardsAssets.brown3,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown4',
+    id: "brown4",
     cardFace: brownCardsAssets.brown4,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown5',
+    id: "brown5",
     cardFace: brownCardsAssets.brown5,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown6',
+    id: "brown6",
     cardFace: brownCardsAssets.brown6,
-    difficulty: 'hard',
-    color:'brown'
+    difficulty: "hard",
+    color: "brown",
   },
   {
-    id: 'brown7',
+    id: "brown7",
     cardFace: brownCardsAssets.brown7,
-    difficulty: 'hard',
-    color:'brown'
+    difficulty: "hard",
+    color: "brown",
   },
   {
-    id: 'brown8',
+    id: "brown8",
     cardFace: brownCardsAssets.brown8,
-    difficulty: 'hard',
-    color:'brown'
+    difficulty: "hard",
+    color: "brown",
   },
   {
-    id: 'brown9',
+    id: "brown9",
     cardFace: brownCardsAssets.brown9,
-    difficulty: 'hard',
-    color:'brown'
+    difficulty: "hard",
+    color: "brown",
   },
   {
-    id: 'brown10',
+    id: "brown10",
     cardFace: brownCardsAssets.brown10,
-    difficulty: 'hard',
-    color:'brown'
+    difficulty: "hard",
+    color: "brown",
   },
   {
-    id: 'brown11',
+    id: "brown11",
     cardFace: brownCardsAssets.brown11,
-    difficulty: 'easy',
-    color:'brown'
+    difficulty: "easy",
+    color: "brown",
   },
   {
-    id: 'brown12',
+    id: "brown12",
     cardFace: brownCardsAssets.brown12,
-    difficulty: 'easy',
-    color:'brown'
+    difficulty: "easy",
+    color: "brown",
   },
   {
-    id: 'brown13',
+    id: "brown13",
     cardFace: brownCardsAssets.brown13,
-    difficulty: 'easy',
-    color:'brown'
+    difficulty: "easy",
+    color: "brown",
   },
   {
-    id: 'brown14',
+    id: "brown14",
     cardFace: brownCardsAssets.brown14,
-    difficulty: 'easy',
-    color:'brown'
+    difficulty: "easy",
+    color: "brown",
   },
   {
-    id: 'brown15',
+    id: "brown15",
     cardFace: brownCardsAssets.brown15,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown16',
+    id: "brown16",
     cardFace: brownCardsAssets.brown16,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown17',
+    id: "brown17",
     cardFace: brownCardsAssets.brown17,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown18',
+    id: "brown18",
     cardFace: brownCardsAssets.brown18,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown19',
+    id: "brown19",
     cardFace: brownCardsAssets.brown19,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown20',
+    id: "brown20",
     cardFace: brownCardsAssets.brown20,
-    difficulty: 'normal',
-    color:'brown'
+    difficulty: "normal",
+    color: "brown",
   },
   {
-    id: 'brown21',
+    id: "brown21",
     cardFace: brownCardsAssets.brown21,
-    difficulty: 'easy',
-    color:'brown'
+    difficulty: "easy",
+    color: "brown",
   },
-]
+];
 
-export default cardsData
+export default cardsData;
---
+++ index.js
@@ -0,0 +1,231 @@
+import ancientsData from "./data/ancients.js";
+import greenCardsData from "./data/mythicCards/green/index.js";
+import brownCardsData from "./data/mythicCards/brown/index.js";
+import blueCardsData from "./data/mythicCards/blue/index.js";
+
+const root = document.getElementById("root");
+
+function getRootTemplate() {
+  return `<div class="ancients-container"> ${getAncientsContainer()}</div>
+    <div class="difficulty-container hidden" id="difficultyContainer">${getButtons()} </div>
+    <div class="deck-container"></div>`;
+}
+
+function getAncientsContainer() {
+  const data = ancientsData.reduce((acc, elem, index) => {
+    return `${acc} <input type='button' class="ancient-card" id=${elem.id} style="background-image: url(${elem.cardFace});"/ >
+        `;
+  }, "");
+  return data;
+}
+
+function initApp() {
+  root.innerHTML = getRootTemplate();
+}
+
+initApp();
+
+function getButtons() {
+  return `<button type="text" class="difficulty" id="easy">Easy </button>
+      <button type="text" class="difficulty" id="normal">Middle</button>
+      <button type="text" class="difficulty" id="hard">Difficult</button>`;
+}
+
+function getButtonDeck() {
+  return `<button type="text" class="shuffle-button ">Shuffle the deck </button> `;
+}
+function getDeck() {
+  return `<div class="current-state ">${currentStateElem()}</div>
+    <div class="deck "></div>
+    <div class="last-card "></div>
+    `;
+}
+const stageMaper = {
+  0: "First stage",
+  1: "Second stage",
+  2: "Third stage",
+};
+function currentStateElem() {
+  return template.reduce(function (acc, elem, index) {
+    return (
+      acc +
+      `<div class="stage-container">
+    <span class="stage-text">${stageMaper[index]}</span>
+    <div class="dots-container">
+    <div class="dot green">${elem.greenCards}</div>
+    <div class="dot brown">${elem.brownCards}</div>
+    <div class="dot blue">${elem.blueCards}</div>
+    </div>
+  
+    </div>`
+    );
+  }, "");
+}
+
+const ancientsContainer = document.querySelector(".ancients-container");
+const difficultyContainer = document.querySelector(".difficulty-container");
+
+let template;
+let difficulty;
+let matrix = [];
+let currentRow = 0;
+let ancient = "";
+
+const deckContainer = document.querySelector(".deck-container");
+const difficultyBtns = document.querySelectorAll(".difficulty");
+
+function addSelectorCardStyle(id) {
+  const ancientCards = document.querySelectorAll(".ancient-card");
+  ancientCards.forEach(function (elem) {
+    if (elem.id == id) {
+      elem.classList.add("border");
+    } else {
+      elem.classList.remove("border");
+    }
+  });
+}
+function showDifficultyBtns(event) {
+  if (ancient == event.target.id) {
+    return;
+  }
+  if (ancient) {
+    deckContainer.innerHTML = getButtonDeck();
+    const shuffleButton = document.querySelector(".shuffle-button");
+    shuffleButton.onclick = shuffleCards;
+  }
+  ancient = event.target.id;
+  addSelectorCardStyle(event.target.id);
+  if (event.target.matches(".ancient-card")) {
+    difficultyContainer.classList.remove("hidden");
+    const selectedAncientsData = ancientsData.find(
+      (elem) => elem.id == event.target.id
+    );
+
+    template = [
+      selectedAncientsData.firstStage,
+      selectedAncientsData.secondStage,
+      selectedAncientsData.thirdStage,
+    ];
+  }
+}
+
+function shuffleCards() {
+  generateMatrix();
+  deckContainer.innerHTML = `<div class="deck-containersArray">${getDeck()}</div>`;
+  const deckCart = document.querySelector(".deck");
+  deckCart.addEventListener("click", handleClickOnDeckCard);
+}
+
+function chooseDifficulty(event) {
+  deckContainer.innerHTML = getButtonDeck();
+  const shuffleButton = document.querySelector(".shuffle-button");
+  shuffleButton.onclick = shuffleCards;
+  difficulty = event.target.id;
+  addSelectorButton();
+}
+difficultyBtns.forEach(function (button) {
+  button.addEventListener("click", chooseDifficulty);
+});
+
+function addSelectorButton() {
+  difficultyBtns.forEach(function (button) {
+    if (difficulty == button.id) {
+      button.classList.add("color");
+    } else {
+      button.classList.remove("color");
+    }
+  });
+}
+ancientsContainer.addEventListener("click", showDifficultyBtns);
+
+function getEasyCards(cardsData) {
+  return cardsData.filter(
+    (elem) => elem.difficulty == "easy" || elem.difficulty == "normal"
+  );
+}
+
+function getNormalCards(cardsData) {
+  return cardsData;
+}
+
+function getHardCards(cardsData) {
+  return cardsData.filter(
+    (elem) => elem.difficulty == "hard" || elem.difficulty == "normal"
+  );
+}
+
+const shuffledArray = (array) => array.sort((a, b) => 0.5 - Math.random());
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const difficultyMapper = {
+  easy: getEasyCards,
+  normal: getNormalCards,
+  hard: getHardCards,
+};
+function arrayGreitCards(cardsNumber, filterCards, row) {
+  new Array(cardsNumber).fill(0).forEach(() => {
+    const number = getRandomArbitrary(0, filterCards.length);
+
+    const card = filterCards.splice(number, 1)[0];
+    row.push(card);
+  });
+}
+
+function generateMatrix() {
+  matrix = [];
+  currentRow = 0;
+  const filtergreenCards = difficultyMapper[difficulty](greenCardsData);
+  const filterbrownCards = difficultyMapper[difficulty](brownCardsData);
+  const filterblueCards = difficultyMapper[difficulty](blueCardsData);
+
+  template.forEach((templ) => {
+    const greenCardsNumber = templ.greenCards;
+    const brownCardsNumber = templ.brownCards;
+    const blueCardsNumber = templ.blueCards;
+
+    const row = [];
+
+    arrayGreitCards(greenCardsNumber, filtergreenCards, row);
+    arrayGreitCards(brownCardsNumber, filterbrownCards, row);
+    arrayGreitCards(blueCardsNumber, filterblueCards, row);
+
+    matrix.push(row);
+  });
+
+  console.log(template);
+}
+
+function handleClickOnDeckCard() {
+  if (!matrix[currentRow].length) {
+    currentRow++;
+  }
+  if (currentRow == 3) {
+    document.querySelector(".deck").style.visibility = "hidden";
+    return;
+  }
+
+  const currentArray = matrix[currentRow];
+  shuffledArray(currentArray);
+  const card = currentArray.splice(0, 1)[0];
+  showCard(card);
+
+  const obj = template[currentRow];
+  obj[`${card.color}Cards`] = obj[`${card.color}Cards`] - 1;
+  updateMatrix();
+
+  console.log(template);
+}
+
+function showCard(card) {
+  const lastCard = document.querySelector(".last-card");
+
+  lastCard.style.backgroundImage = `url(${card.cardFace})`;
+}
+
+function updateMatrix() {
+  const currentState = document.querySelector(".current-state");
+  currentState.innerHTML = currentStateElem();
+}
